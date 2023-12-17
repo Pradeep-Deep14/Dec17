14c089e7062b461f974a8cd9dc607c656c8646e5
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-12-17 10:14:39 +0530

    first commit

--- 1.py
+++ 1.py
@@ -1,2 +1,5 @@
 g=( i for i in range(5))
 print(type(g))
+
+
+#Class <generator>
---
+++ 2.py
@@ -0,0 +1,15 @@
+def outer_func(x):
+    y=1+x
+    def inner_func():
+        x=2
+        def inner_inner():
+            x=3
+            return x+y
+        return inner_inner
+    return inner_func
+
+results=outer_func(4)
+my_results=results()
+print(my_results()+2)
+
+#10
